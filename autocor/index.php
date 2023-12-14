895c42a91a524855bbaa54adf4d7aef024f7a98d
Ftach0 <98447425+Ftach0@users.noreply.github.com>
2023-12-14 15:09:23 +0300

    Add files via upload

---
+++ autocor/index.php
@@ -0,0 +1,17 @@
+<?php
+if (!empty($_POST["inputText"])){
+$words = ["дорога"=>"road","путь"=>"way","машина"=>"car" ];
+foreach ($words as $ru => $en){
+    $inputText = str_replace($ru,$en,$inputText);
+}
+echo $inputText;
+}
+?>
+<form action="" method="Post">
+    <br>
+    <textarea name="inputText"></textarea>
+    <br>
+    <input type="submit"> 
+
+</form>
+
